249695d7e4bb2f1ce883fe5c67d305372147780a
annadavydova <lileynik@yahoo.com>
2023-02-06 12:59:00 -0500

    initional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number1, setNumber1] = useState(10)
+    const [number2, setNumber2] = useState(10)
+
+    const changeNumber1 = (event) => {
+        console.log(typeof event.target.value)
+        event.target.value == +event.target.value
+            ? event.target.value = event.target.value
+            : event.target.value = 0
+        setNumber1(+event.target.value)
+    }
+    const changeNumber2 = (event) => {
+        event.target.value == +event.target.value
+            ? event.target.value = event.target.value
+            : event.target.value = 0
+        setNumber2(+event.target.value)
+    }
+
+    return (
+        <div>
+            <input value={number1} onChange={changeNumber1}/>
+            <input value={number2} onChange={changeNumber2}/>
+            <hr/>
+            <br/> <strong> Plus: </strong> {number1} + {number2} = {number1 + number2}
+            <br/> <strong> Minus:</strong> {number1} - {number2} = {number1 - number2}
+            <br/> <strong> Multiplication: </strong> {number1} * {number2} = {number1 * number2}
+
+            <br/> <strong>Division: </strong> {number1} / {number2} = {number1 / number2}
+
+
+        </div>
+    );
 }
 
 export default App;
